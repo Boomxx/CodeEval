c0516a8e4f1cbd41eb1941b3b005767dbf5ca5a7
Boomxx <bath.anthony@gmail.com>
2016-04-06 20:38:33 -0400

    Added N Mod M solution

---
+++ CE_Easy_NModM.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+class CE_Easy_NModM
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                var parts = line.Split(',');
+                System.Console.WriteLine(Mod(System.Convert.ToInt16(parts[0]), System.Convert.ToInt16(parts[1])));
+            }
+    }
+
+    private static int Mod(int N, int M)
+    {
+        return N - (N / M) * M;
+    }
+}
