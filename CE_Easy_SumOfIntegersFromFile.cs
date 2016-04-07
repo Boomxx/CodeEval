2ce2a8af75d908d1572c974c3ddabd52a7ee05d3
Boomxx <bath.anthony@gmail.com>
2016-04-06 20:41:31 -0400

    Added Sum of Integers From File solution

---
+++ CE_Easy_SumOfIntegersFromFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class CE_Easy_SumOfIntegersFromFile
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                sum += Convert.ToInt16(line);
+            }
+
+        Console.Write(sum);
+    }
+}
