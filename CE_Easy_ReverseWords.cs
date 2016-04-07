b952b22135ee9c30a5a677dfef866467df0900d7
Boomxx <bath.anthony@gmail.com>
2016-04-06 20:51:20 -0400

    Added Reverse Words solution

---
+++ CE_Easy_ReverseWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class CE_Easy_ReverseWords
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                Console.WriteLine(string.Join(" ", ReverseWords(line)));
+            }
+    }
+
+    private static string[] ReverseWords(string line)
+    {
+        var words = line.Split(' ');
+        Array.Reverse(words);
+
+        return words;
+    }
+}
