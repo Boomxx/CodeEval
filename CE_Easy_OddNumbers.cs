5f15013a7488c6e1fbc6db513c8fa430586e44fd
Boomxx <bath.anthony@gmail.com>
2016-04-06 20:40:03 -0400

    Added Odd Numbers solution

---
+++ CE_Easy_OddNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class CE_Easy_OddNumbers
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 99; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
