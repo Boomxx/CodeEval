79770115876007ef53af9bfc857c5fa9ace1ceb5
Boomxx <bath.anthony@gmail.com>
2016-04-06 21:09:11 -0400

    Rename CE_Easy_MultiplicationTables.cs to Easy/CE_Easy_MultiplicationTables.cs

---
+++ Easy/CE_Easy_MultiplicationTables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class CE_Easy_MultiplicationTables
+{
+    static void Main(string[] args)
+    {
+        PrintMultiplicationTables(12);
+    }
+
+    private static void PrintMultiplicationTables(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (j == 1)
+                    Console.Write(string.Format("{0,4}", i * j).TrimStart());
+                else
+                    Console.Write(string.Format("{0,4}", i * j));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
