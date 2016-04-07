b90d38040214dbdd9e93fe9a6a2561c8f8d0dcdb
Boomxx <bath.anthony@gmail.com>
2016-04-06 20:45:51 -0400

    Added Sum of Digits solution

---
+++ CE_Easy_SumOfDigits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class CE_Easy_SumOfDigits
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                Console.WriteLine(SumOfDigits(line));
+            }
+    }
+
+    private static int SumOfDigits(string line)
+    {
+        int sum = 0;
+
+        foreach (char d in line)
+        {
+            sum += d - '0';
+        }
+
+        return sum;
+    }
+}
