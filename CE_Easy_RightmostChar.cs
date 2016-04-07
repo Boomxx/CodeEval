0624ea2c597373e6149934e60ec41c6346c9c37d
Boomxx <bath.anthony@gmail.com>
2016-04-06 20:34:51 -0400

    Added Righmost Char solution

---
+++ CE_Easy_RightmostChar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class CE_Easy_RightmostChar
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+
+                var parts = line.Split(',');
+                Console.WriteLine(parts[0].LastIndexOf((char)parts[1][0]));
+            }
+    }
+}
